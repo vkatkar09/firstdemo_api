18c94efd5cd03f5455fc6a18fb43071f5ec5f94c
Chandrashekar Dashudu <cddashud@ncsu.edu>
2019-06-16 01:34:18 -0700

    added history

--- api/releases/controller.js
+++ api/releases/controller.js
@@ -4,46 +4,72 @@ const fs = require('fs');
 /*
 Varsha's controller functions
 */
-exports.getUser = function(req, res, next) {
+
+var commonPath= "/Users/dashuduc/Documents/varsha/api"
+exports.getUser = function (req, res, next) {
     var username = req.params.username;
     var password = req.params.password;
-    
-    fs.writeFile ("input.json", JSON.stringify(data), function(err) {
-    if (err) throw err;
-    console.log('complete');
+
+    fs.writeFile("input.json", JSON.stringify(data), function (err) {
+        if (err) throw err;
+        console.log('complete');
     })
     res.json(respBody);
 }
 
-exports.login = function(req, res, next) {
+exports.login = function (req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
     var token = false;
-    fs.readFile('/Users/dashuduc/Documents/varsha/api/api/releases/users.json', 'utf8',(err, fileContent) => {
-        if( err ) {
-            res.json({"sucess": "false"});
+    fs.readFile(commonPath + '/api/releases/users.json', 'utf8', (err, fileContent) => {
+        if (err) {
+            res.json({ "sucess": "false" });
         } else {
-          data = JSON.parse(fileContent.toString());
-          
-          data.forEach(function(user){
-            if (user.username == username && user.password == password){
-                token = true
-            }
-          })
+            data = JSON.parse(fileContent.toString());
+
+            data.forEach(function (user) {
+                if (user.username == username && user.password == password) {
+                    token = true
+                }
+            })
 
 
-          res.json({"sucess": token});
-          
+            res.json({ "sucess": token });
+
         }
 
     })
-    
+}
+exports.getHistory = function(req, res){
+    fs.readFile(commonPath + '/api/releases/history.json', 'utf8', (err, fileContent) => {
+        if (err) {
+        } else {
+            data = JSON.parse(fileContent.toString());
+            res.json(data);
+        }
+    })
+}
 
+exports.saveData = function (req, res) {
+    console.log(req.body)
+    fs.readFile(commonPath + '/api/releases/history.json', 'utf8', (err, fileContent) => {
+        if (err) {
+        } else {
+            data = JSON.parse(fileContent.toString());
+            data.push(req.body);
+
+            fs.writeFile(commonPath + "/api/releases/history.json", JSON.stringify(data), function (err) {
+                if (err) throw err;
+                console.log('complete');
+                res.json({ "sucess": "true" });
+            })
+        }
+    })
     
 }
 
-exports.register = function(req, res) {
-    
+exports.register = function (req, res) {
+
     var username = req.body.username;
     var password = req.body.password;
     var email = req.body.email;
@@ -52,92 +78,91 @@ exports.register = function(req, res) {
         "username": username,
         "password": password,
         "email": email,
-        "contact": contact 
+        "contact": contact
     }
-    
-    fs.readFile('/Users/dashuduc/Documents/varsha/api/api/releases/users.json', 'utf8',(err, fileContent) => {
-        if( err ) {
+
+    fs.readFile(commonPath + '/api/releases/users.json', 'utf8', (err, fileContent) => {
+        if (err) {
         } else {
-          data = JSON.parse(fileContent.toString());
-          console.log(fileContent);
-          data.push(userObject);
-          fs.writeFile ("/Users/dashuduc/Documents/varsha/api/api/releases/users.json", JSON.stringify(data), function(err) {
-            if (err) throw err;
-            console.log('complete');
-            res.json({"sucess": "true"});
+            data = JSON.parse(fileContent.toString());
+            console.log(fileContent);
+            data.push(userObject);
+            fs.writeFile(commonPath + "/api/releases/users.json", JSON.stringify(data), function (err) {
+                if (err) throw err;
+                console.log('complete');
+                res.json({ "sucess": "true" });
             })
         }
     })
 
- 
+
 }
 
-exports.addToList = function(req, res, next) {
+exports.addToList = function (req, res, next) {
     var name = req.body.name;
     var description = req.body.description;
-    
+
     var list = {
         "name": name,
         "description": description
     }
-    
-    fs.readFile('/Users/dashuduc/Documents/varsha/api/api/releases/todolist.json', 'utf8',(err, fileContent) => {
-        if( err ) {
+
+    fs.readFile(commonPath + '/api/releases/todolist.json', 'utf8', (err, fileContent) => {
+        if (err) {
         } else {
-          data = JSON.parse(fileContent.toString());
-          data.push(list);
-          fs.writeFile ("/Users/dashuduc/Documents/varsha/api/api/releases/todolist.json", JSON.stringify(data), function(err) {
-            if (err) throw err;
-            console.log('complete');
-            res.json({"sucess": "true"});
+            data = JSON.parse(fileContent.toString());
+            data.push(list);
+            fs.writeFile(commonPath + "/api/releases/todolist.json", JSON.stringify(data), function (err) {
+                if (err) throw err;
+                console.log('complete');
+                res.json({ "sucess": "true" });
             })
         }
     })
 }
 
-exports.getToDoList = function(req, res, next) {
-    fs.readFile('/Users/dashuduc/Documents/varsha/api/api/releases/todolist.json', 'utf8',(err, fileContent) => {
-        if( err ) {
-            
+exports.getToDoList = function (req, res, next) {
+    fs.readFile(commonPath + '/api/releases/todolist.json', 'utf8', (err, fileContent) => {
+        if (err) {
+
         } else {
-          data = JSON.parse(fileContent.toString());
-          res.json(data);
-          
+            data = JSON.parse(fileContent.toString());
+            res.json(data);
+
         }
 
     })
 }
 
-exports.deleteFromList = function(req, res, next) {
-    var name  = req.body.name;
+exports.deleteFromList = function (req, res, next) {
+    var name = req.body.name;
     console.log(req.body)
-    fs.readFile('/Users/dashuduc/Documents/varsha/api/api/releases/todolist.json', 'utf8',(err, fileContent) => {
-        if( err ) {
-            res.json({"sucess": "false"});
+    fs.readFile(commonPath + '/api/releases/todolist.json', 'utf8', (err, fileContent) => {
+        if (err) {
+            res.json({ "sucess": "false" });
         } else {
-          data = JSON.parse(fileContent.toString());
-          for (var i = 0; i < data.length; i++) {
-            if (data[i].name == name)
-            {
-                console.log(data[i].name);
-              data.splice(i,1)
-              fs.writeFile ("/Users/dashuduc/Documents/varsha/api/api/releases/todolist.json", JSON.stringify(data), function(err) {
-                if (err) throw err;
-                console.log('complete');
-                })
+            data = JSON.parse(fileContent.toString());
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].name == name) {
+                    console.log(data[i].name);
+                    data.splice(i, 1)
+                    fs.writeFile(commonPath + "/api/releases/todolist.json", JSON.stringify(data), function (err) {
+                        if (err) throw err;
+                        console.log('complete');
+                    })
+                }
             }
-          }
-        //   data.forEach(function(list){
-        //       console.log(list);
-        //     if(list.name == name){
-        //         data.pop(list)
-        //     }
-        //   })
-          
+            //   data.forEach(function(list){
+            //       console.log(list);
+            //     if(list.name == name){
+            //         data.pop(list)
+            //     }
+            //   })
+
         }
 
     })
-    res.json({"sucess": "true"});
+    res.json({ "sucess": "true" });
 }
 
 
--- api/releases/index.js
+++ api/releases/index.js
@@ -1,6 +1,6 @@
 var express = require('express')
-    
-var  controller = require('./controller');
+
+var controller = require('./controller');
 
 var router = express.Router();
 
@@ -13,6 +13,12 @@ router.route('/users')
 
 router.route('/login')
     .post(controller.login);
+
+router.route('/save')
+    .post(controller.saveData);
+
+router.route('/save')
+    .get(controller.getHistory);
 
 router.route('/register')
     .post(controller.register);
